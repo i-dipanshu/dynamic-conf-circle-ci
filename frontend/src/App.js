5b0bb4d1351e7721197e0c1cee8f13edaa9d18bc
Dipanshu <medipansh@gmail.com>
2023-10-14 23:26:32 +0530

    trigger frontend
    Signed-off-by: Dipanshu <medipansh@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react 
+          Learn React 
         </a>
       </header>
     </div>
